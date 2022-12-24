2171a034c462ef581c98d46ac95ac4e844af7ef1
Awais Jameel <awais.jameel@ymail.com>
2022-12-24 23:04:59 +0500

    VHost Setup script updated.

--- Bash Script to Create VHost & Install SSL on ubuntu20.sh
+++ Bash Script to Create VHost & Install SSL on ubuntu20.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set the script to exit if any commands fail
+set -e
+set -o pipefail
+
 # Check if Apache2 is installed
 if ! which apache2 > /dev/null; then
   echo "Apache2 is not installed. Please install Apache2 and try again."
@@ -9,63 +13,73 @@ fi
 # Check if OpenSSL is installed
 if ! which openssl > /dev/null; then
   echo "OpenSSL is not installed. Installing OpenSSL..."
-  apt-get update
-  apt-get install -y openssl
+  apt update
+  apt install -y openssl
 fi
 
 # Enable the SSL module for Apache
 a2enmod ssl
 
 # Set the domain name and document root
-read -r -p "Enter the domain name for the virtual host: " domain
-read -r -p "Enter the document root for the virtual host: " doc_root
+read -r -p "Enter the domain name for the virtual host: " -t 180 domain
+read -r -p "Enter the document root for the virtual host: " -t 180 doc_root
+
+# Check if the virtual host configuration file already exists
+if [ -f "/etc/apache2/sites-available/${domain}.conf" ]; then
+  echo "Virtual host configuration file already exists. Exiting script."
+  exit 1
+fi
 
 # Create the virtual host configuration file
 echo "Creating virtual host configuration file..."
 
-cat > /etc/apache2/sites-available/$domain.conf <<EOL
+cat > "/etc/apache2/sites-available/${domain}.conf" <<EOL
 <VirtualHost *:80>
-  ServerName $domain
+  ServerName ${domain}
   ServerAdmin webmaster@localhost
-  DocumentRoot $doc_root
+  DocumentRoot ${doc_root}
   ErrorLog \${APACHE_LOG_DIR}/error.log
   CustomLog \${APACHE_LOG_DIR}/access.log combined
+  Redirect permanent / https://${domain}/
 </VirtualHost>
 EOL
 
 # Enable the virtual host and disable the default host
 echo "Enabling virtual host and disabling default host..."
-a2ensite $domain.conf
+a2ensite "${domain}.conf"
 a2dissite 000-default.conf
 
-# Generate a self-signed SSL certificate
-echo "Generating self-signed SSL certificate..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/ssl/private/$domain.key -out /etc/ssl/certs/$domain.crt \
-  -subj "/CN=$domain"
+# Check if the SSL virtual host configuration file already exists
+if [ -f "/etc/apache2/sites-available/${domain}-ssl.conf" ]; then
+  echo "SSL virtual host configuration file already exists. Exiting script."
+  exit 1
+fi
 
 # Create the SSL virtual host configuration file
 echo "Creating SSL virtual host configuration file..."
 
-cat > /etc/apache2/sites-available/$domain-ssl.conf <<EOL
+cat > "/etc/apache2/sites-available/${domain}-ssl.conf" <<EOL
 <IfModule mod_ssl.c>
   <VirtualHost *:443>
-    ServerName $domain
+    ServerName ${domain}
     ServerAdmin webmaster@localhost
-    DocumentRoot $doc_root
+    DocumentRoot ${doc_root}
     ErrorLog \${APACHE_LOG_DIR}/error.log
     CustomLog \${APACHE_LOG_DIR}/access.log combined
     SSLEngine on
-    SSLCertificateFile /etc/ssl/certs/$domain.crt
-    SSLCertificateKeyFile /etc/ssl/private/$domain.key
+    SSLCertificateFile /etc/ssl/certs/${domain}.crt
+    SSLCertificateKeyFile /etc/ssl/private/${domain}.key
+    Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains; preload"
   </VirtualHost>
 </IfModule>
 EOL
 
 # Enable the SSL virtual host
 echo "Enabling SSL virtual host..."
-a2ensite $domain-ssl.conf
+a2ensite "${domain}-ssl.conf"
 
 # Reload Apache2 to apply the changes
 echo "Reloading Apache2..."
 systemctl reload apache2
+
+echo "Virtual host setup complete."
